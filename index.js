ec8fcfa503d27a6fbe7cc8adc9fd993afb374e99
Arthur Fernandes Ribeiro <arthur.ribeiro@zup.com.br>
2019-09-29 22:01:09 -0300

    Create topic kafka

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://127.0.0.1')
 const dojotClient = mqtt.connect('mqtt://192.168.0.117')
+const kafka = require('kafka-node'),
 
 returnFirst = function(obj) { for(key in obj){return obj[key];} }
 
@@ -16,10 +17,17 @@ client.on('message', function (topic, message) {
   const messageObj = JSON.parse(message.toString()).object;
   // Object.keys(messageObj).map(function(key, index) {messageObj[key] = returnFirst(messageObj[key])});
   // dojotClient.publish('/admin/4b7a9c/attrs', JSON.stringify(messageObj));
-  const publishPayload = {
-    confirmed: true,
-    fPort: 10,
-    object: messageObj
-  }
-  client.publish('application/1/device/3431373260367a0e/tx', JSON.stringify(publishPayload))
+  // const publishPayload = {
+  //   confirmed: true,
+  //   fPort: 10,
+  //   object: messageObj
+  // }
+  // client.publish('application/1/device/3431373260367a0e/tx', JSON.stringify(publishPayload))
 })
+
+const kafkaClient = kafka.kafkaClient({kafkaHost: '192.168.0.111:9092'})
+var topicsToCreate = [{
+  topic: 'loraTopic',
+  partitions: 1,
+  replicationFactor: 2
+}]

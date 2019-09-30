d6f583cc5cc9a87a644792f86d6704ea62b7a16d
Arthur Fernandes Ribeiro <arthur.ribeiro@zup.com.br>
2019-09-29 22:42:52 -0300

    Test kafkajs

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://127.0.0.1')
 const dojotClient = mqtt.connect('mqtt://192.168.0.117')
-const kafka = require('kafka-node'),
+const { Kafka } = require('kafkajs')
 
 returnFirst = function(obj) { for(key in obj){return obj[key];} }
-Producer = kafka.Producer
-kafkaClient = new kafka.KafkaClient('192.168.0.119:9092')
-kafkaProducer = new Producer(kafkaClient)
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['192.168.9.111:9092', '192.168.0.119:9092', '192.168.0.123:9092']
+})
 
-// Consumer = kafka.Consumer
-// consumer = new Consumer(
-//     kafkaClient,
-//     [
-//         { topic: 'loraTopic', partition: 0 },
-//     ],
-//     {
-//         autoCommit: false
-//     }
-// );
-
-// consumer.on('message', function (message) {
-//   console.log(message);
-// });
+const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: 'test-group' })
+ 
+const run = async () => {
+  // Producing
+  await producer.connect()
+  await producer.send({
+    topic: 'test-topic',
+    messages: [
+      { value: 'Hello KafkaJS user!' },
+    ],
+  })
+ 
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+ 
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+ 
+run().catch(console.error)
 
 // client.on('connect', function () {
 //   client.subscribe('application/1/device/3431373260367a0e/rx', function (err) {
@@ -30,26 +46,15 @@ kafkaProducer = new Producer(kafkaClient)
 //     }
 //   })
 // })
-
-kafkaProducer.on('ready', function () {
-  // client.on('message', function (topic, message) {
-  //   const messageObj = JSON.parse(message.toString()).object;
-  //   const payloads = [
-  //     { topic: 'loraTopic', messages: message,},
-  //   ];
-
-  //   producer.send(payloads, function (err, data) {
-  //       // console.log(data);
-  //   });
-  // })
-  while(true) {
-    setTimeout( ()=> {
-      const payloads = [
-        { topic: 'loraTopic', messages: 'hi',},
-      ];
-      kafkaProducer.send(payloads, function (err, data) {
-        console.log(data);
-      });
-    }, 1000)
-  }
-});
+ 
+// client.on('message', function (topic, message) {
+//   const messageObj = JSON.parse(message.toString()).object;
+//   // Object.keys(messageObj).map(function(key, index) {messageObj[key] = returnFirst(messageObj[key])});
+//   // dojotClient.publish('/admin/4b7a9c/attrs', JSON.stringify(messageObj));
+//   const publishPayload = {
+//     confirmed: true,
+//     fPort: 10,
+//     object: messageObj
+//   }
+//   client.publish('application/1/device/3431373260367a0e/tx', JSON.stringify(publishPayload))
+// })

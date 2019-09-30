0ea63c09ced68236a9888fd65cbfa70a3799bdff
Arthur Fernandes Ribeiro <arthur.ribeiro@zup.com.br>
2019-09-29 22:26:46 -0300

    Comments

--- index.js
+++ index.js
@@ -9,46 +9,48 @@ KeyedMessage = kafka.KeyedMessage
 kafkaClient = new kafka.KafkaClient({kafkaHost: '192.168.0.111:9092'})
 kafkaProducer = new Producer(client)
 
-Consumer = kafka.Consumer
-consumer = new Consumer(
-    kafkaClient,
-    [
-        { topic: 'loraTopic', partition: 0 },
-    ],
-    {
-        autoCommit: false
-    }
-);
+// Consumer = kafka.Consumer
+// consumer = new Consumer(
+//     kafkaClient,
+//     [
+//         { topic: 'loraTopic', partition: 0 },
+//     ],
+//     {
+//         autoCommit: false
+//     }
+// );
 
-consumer.on('message', function (message) {
-  console.log(message);
-});
+// consumer.on('message', function (message) {
+//   console.log(message);
+// });
 
-client.on('connect', function () {
-  client.subscribe('application/1/device/3431373260367a0e/rx', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt');
-    }
-  })
-})
+// client.on('connect', function () {
+//   client.subscribe('application/1/device/3431373260367a0e/rx', function (err) {
+//     if (!err) {
+//       client.publish('presence', 'Hello mqtt');
+//     }
+//   })
+// })
 
 kafkaProducer.on('ready', function () {
-  client.on('message', function (topic, message) {
-    const messageObj = JSON.parse(message.toString()).object;
-    const payloads = [
-      { topic: 'loraTopic', messages: message,},
-    ];
+  // client.on('message', function (topic, message) {
+  //   const messageObj = JSON.parse(message.toString()).object;
+  //   const payloads = [
+  //     { topic: 'loraTopic', messages: message,},
+  //   ];
 
-    producer.send(payloads, function (err, data) {
-        // console.log(data);
-    });
-  })
-  setTimeout( ()=> {
-    const payloads = [
-      { topic: 'loraTopic', messages: 'hi',},
-    ];
-    producer.send(payloads, function (err, data) {
-      // console.log(data);
-    });
-  }, 1000)
+  //   producer.send(payloads, function (err, data) {
+  //       // console.log(data);
+  //   });
+  // })
+  while(true) {
+    setTimeout( ()=> {
+      const payloads = [
+        { topic: 'loraTopic', messages: 'hi',},
+      ];
+      kafkaProducer.send(payloads, function (err, data) {
+        console.log(data);
+      });
+    }, 1000)
+  }
 });

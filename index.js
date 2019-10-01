9a2f41927447ecd0dadb88fd73d66b171cde2718
Arthur Fernandes Ribeiro <arthur.ribeiro@zup.com.br>
2019-09-30 21:44:08 -0300

    Some more tests

--- index.js
+++ index.js
@@ -12,30 +12,30 @@ const kafka = new Kafka({
 const producer = kafka.producer()
 const consumer = kafka.consumer({ groupId: 'test-group' })
  
-// const run = async () => {
-//   // Producing
-//   await producer.connect()
-//   await producer.send({
-//     topic: 'test-topic',
-//     messages: [
-//       { value: 'Hello KafkaJS user!' },
-//     ],
-//   })
+const run = async () => {
+  // Producing
+  await producer.connect()
+  // await producer.send({
+  //   topic: 'test-topic',
+  //   messages: [
+  //     { value: 'Hello KafkaJS user!' },
+  //   ],
+  // })
  
-//   // Consuming
-//   await consumer.connect()
-//   await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+  // // Consuming
+  // await consumer.connect()
+  // await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
  
-//   await consumer.run({
-//     eachMessage: async ({ topic, partition, message }) => {
-//       console.log({
-//         partition,
-//         offset: message.offset,
-//         value: message.value.toString(),
-//       })
-//     },
-//   })
-// }
+  // await consumer.run({
+  //   eachMessage: async ({ topic, partition, message }) => {
+  //     console.log({
+  //       partition,
+  //       offset: message.offset,
+  //       value: message.value.toString(),
+  //     })
+  //   },
+  // })
+}
  
 // run().catch(console.error)
 

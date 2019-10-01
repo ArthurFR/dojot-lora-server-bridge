7bf01e025530d299dc806ff610639b18275278c9
Arthur Fernandes Ribeiro <arthur.ribeiro@zup.com.br>
2019-09-30 22:17:12 -0300

    Trying kafka-node

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
-const mqtt = require('mqtt')
-const client  = mqtt.connect('mqtt://127.0.0.1')
-const dojotClient = mqtt.connect('mqtt://192.168.0.117')
-const { Kafka } = require('kafkajs')
-
+// const mqtt = require('mqtt')
+// const client  = mqtt.connect('mqtt://127.0.0.1')
+// const dojotClient = mqtt.connect('mqtt://192.168.0.117')
+const kafka = require('kafka-node')
 returnFirst = function(obj) { for(key in obj){return obj[key];} }
-const kafka = new Kafka({
-  clientId: 'my-app',
-  brokers: ['192.168.9.111:9092', '192.168.0.119:9092', '192.168.0.123:9092']
-})
 
-const producer = kafka.producer()
-const consumer = kafka.consumer({ groupId: 'test-group' })
- 
-const run = async () => {
-  // Producing
-  await producer.connect()
-  await producer.send({
-    topic: 'test-topic',
-    messages: [
-      { value: 'Hello KafkaJS user!' },
-    ],
-  })
- 
-  // // Consuming
-  // await consumer.connect()
-  // await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
- 
-  // await consumer.run({
-  //   eachMessage: async ({ topic, partition, message }) => {
-  //     console.log({
-  //       partition,
-  //       offset: message.offset,
-  //       value: message.value.toString(),
-  //     })
-  //   },
-  // })
-}
- 
-run().catch(console.error)
+const client = new kafka.KafkaClient({kafkaHost: '192.168.0.111:9092'});
+Producer = kafka.Producer,
+client = new kafka.KafkaClient(),
+producer = new Producer(client);
+
+payloads = [
+  { topic: 'topic1', messages: 'hi', partition: 0 },
+  { topic: 'topic2', messages: ['hello', 'world', km] }
+];
+producer.on('ready', function () {
+producer.send(payloads, function (err, data) {
+  console.log(data);
+});
+});
+
+producer.on('error', function (err) {})
 
 // client.on('connect', function () {
 //   client.subscribe('application/1/device/3431373260367a0e/rx', function (err) {

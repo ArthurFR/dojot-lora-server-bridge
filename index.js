c45ae987e79a38e9dd04be3027a8a41cb39252a6
Arthur Fernandes Ribeiro <arthur.ribeiro@zup.com.br>
2019-10-02 13:19:27 -0300

    Change program to send received data from lora to kafka

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+const mqtt = require('mqtt')
 var kafka = require("kafka-node"),
   Producer = kafka.Producer,
-  client = new kafka.KafkaClient({kafkaHost: '192.168.0.111:9092'}),
-  producer = new Producer(client);
+  clientKafka = new kafka.KafkaClient({kafkaHost: '192.168.0.111:9092'}),
+  producer = new Producer(clientKafka);
 
-let count = 0;
+returnFirst = function(obj) { for(key in obj){return obj[key];} }
 
 producer.on("ready", function() {
-  console.log("ready");
-  setInterval(function() {
-    payloads = [
-      { topic: "loraTopic", messages: `I have ${count} cats`, partition: 0 }
-    ];
+  const loraClient = mqtt.connect('mqtt://127.0.0.1')
+  loraClient.on('connect', function () {
+    loraClient.subscribe('application/1/device/3431373260367a0e/rx', function (err) {})
+  })
 
-    producer.send(payloads, function(err, data) {
-      console.log(data);
-      count += 1;
-    });
-  }, 5000);
+  loraClient.on('message', function (topic, message) {
+    const messageObj = JSON.parse(message.toString()).object;
+    payloads = [{ topic: "loraTopic", messages: JSON.stringify(messageObj), partition: 0 }];
+
+    producer.send(payloads, function(err, data) {});
+  })
 });
 
 producer.on("error", function(err) {
   console.log(err);
 });
+
+
+ 
+

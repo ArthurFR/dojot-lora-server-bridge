640ed2ad159d51ce1ddcda18fe6bc5fffe257471
Arthur Fernandes Ribeiro <arthur.ribeiro@zup.com.br>
2019-10-22 13:51:21 -0300

    Fix kafka topic

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ var kafka = require("kafka-node"),
     autoCommit: false
   });
 
-//var topicsToCreate = [{
-//  topic: 'loraDown',
-//  partitions: 1,
-//  replicationFactor: 2
-//}]
-
-//clientKafka.createTopics(topicsToCreate, (error, result) => {
-  // result is an array of any errors if a given topic could not be created
-//});
-
 consumer.on('message', function(message) {
   console.log(message);
 });
@@ -33,14 +23,14 @@ producer.on("ready", function() {
     loraClient.subscribe('application/1/device/3431373260367a0e/rx', function (err) {})
 
     setInterval(() => {
-      const payloads2 = [{ topic: "loraTopic", messages: 'JSON.stringify(messageObj)', partition: 0 }];
+      const payloads2 = [{ topic: "loraDown", messages: 'JSON.stringify(messageObj)', partition: 0 }];
       producer.send(payloads2, function(err, data) {});
     }, 10000)
   })
 
   loraClient.on('message', function (topic, message) {
     const messageObj = JSON.parse(message.toString()).object;
-    payloads = [{ topic: "loraDown", messages: JSON.stringify(messageObj), partition: 0 }];
+    payloads = [{ topic: "loraTopic", messages: JSON.stringify(messageObj), partition: 0 }];
 
     const publishPayload = {
       confirmed: true,
